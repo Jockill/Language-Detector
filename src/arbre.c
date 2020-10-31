04f15b0879bf646c22ea235052ea6cfab5c3e90b
Wernert Thomas <thomas.wernert@etu.unistra.fr>
2020-10-31 17:16:53 +0100

    Modification profonde de arbre.c

--- src/arbre.c
+++ src/arbre.c
@@ -4,90 +4,114 @@
 #include "../header/utils.h"
 #include "../header/hashmap.h"
 
-#define N 100
+#define N 25
 
 typedef struct Noeud {
-	char *data; 
-	bool flag; // Si ds le mot est ds le dicou ou non 
-	struct Noeud **tab; // Tab vers ses fils 
+
+	//char *data; // Les mots 
+	//bool flag; // Savoir si mot du dictionnaire Pr la phrase
+	struct Noeud *tab[N]; // Tableau des lettres a-z 
+	bool feuille; // Terminaison d'une branche et dico ? 
 } Noeud; 
 
 typedef struct Noeud *Arbre; 
 
 
 
-// Créer t-il un arbre à chaque fois ? 
+// Manque data et flag 
 Arbre noeud_initialisation(void) {
-	Arbre t = (Arbre)malloc(sizeof(Noeud));
-	if(t == NULL) perror("Erreur malloc"); 
-	t -> data = NULL; 
-	t -> flag = false; 
-	t -> tab = malloc(26 * sizeof(Arbre));
-	for(int i = 0; i < 26; i++) {
-		t -> tab[i] = NULL; 
+	Arbre nouveau_noeud = (Arbre)malloc(sizeof(Noeud)); 
+	nouveau_noeud -> feuille = false; 
+	for(int i = 0; i <= N; i++) {
+		nouveau_noeud -> tab[i] = NULL; 
 	}
-	return t; 
+	return nouveau_noeud; 
 }
 
 
 
+void noeud_insertion(Noeud *racine, char *message) {
+	Arbre chemin = racine; 
+	
+	
+	while(*message) {
+		// Création d'un nv noeud si le chemin n'existe pas 
+		if(chemin -> tab[*message - 'a'] == NULL) {
+			chemin -> tab[*message - 'a'] = noeud_initialisation(); 
+		}
+		chemin = chemin -> tab[*message - 'a']; 
+		message++; 
+	}
+	chemin -> feuille = true; 
+}
 
-// Si index sup à 25 devient 0 
-int index_mod(int index) {
-	if(index > 25) {
-		index = 0;
+
+
+// Pr savoir si est mot du dico 
+bool recherche_mot_arbre(Arbre racine, char *message) {
+	bool result = false; 
+	if(racine == NULL) {
+		return result; 
+	}
+	Arbre chemin = racine; 
+	while(*message) {
+
+		// Pr aller au noeud suivant 
+		chemin = chemin -> tab[*message - 'a']; 
+		
+		// Test si le noeud suivant est existant 
+		if(chemin == NULL) {
+			return result; 
+		}
+
+		message++; 
 	} 
-	return index; 
+	// Si le chemin noeud est une feuille et nous avons atteint 
+	// la fin du message nous retournons vrai 
+	result = chemin -> feuille; 
+	return result; 
 }
 
 
 
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// 2 fonctions init (arbre puis noeud ????)
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-// Faire appel à noeud_init et celle de test de mot 
-void noeud_insertion_lettre(Arbre arbre, char lettre, int index) {
-	//Arbre nul
-	if(arbre == NULL) perror("Prbm arbre nul"); 
-	//Boucle/récu qui parcours le tab pour ajouter 
-	Noeud *nouv_noeud; 
-	nouv_noeud = noeud_initialisation(); 
-	nouv_noeud -> data = lettre; // Manque les lettres précédentes 
-
-
-
-	// Pointe  vers le prochain fils à remplir ???? Non nécessaire si j'avais 2 fonctions d'init 
-	nouv_noeud -> tab[index_mod(index + 1)] = ;  
-
-
-}
-
-void noeud_insertion_mot(Arbre arbre, char *mot) {
-	// Nb de caract du mot 
-	char buf_ascii[N]; // Nous plaçons les ascii ici 
-	int buf_index[N]; // Nous plaçons les index ici 
-	sprintf(buf_ascii, "%s", mot); 
-	for(int i = 0; i < N; i++) {
-		buf_index[i] = ascii_to_index(buf_ascii[i]); // Transvase les ascii tab ds index tab 
-		// Nécessaire pour savoir écire ds quel tab (rappel 26 tabs)
-		noeud_insertion_lettre(arbre, buf_ascii[i], buf_index[i]); 
+// Voir si un noeud possède un noeud fils 
+bool possede_fils(Arbre chemin) {
+	size_t cptr = 0;
+	bool result = false; 
+	while(cptr < N) {
+		// Si fils 
+		if(chemin -> tab[cptr]) {
+			result = true; 
+			return result; 
+		}
 	}
+	return result; 
 }
 
 
-void recherche_mot_arbre(char *mot) {
-}
 
 
-
-void print_arbre(Noeud *arbre) {
-
-}
 
 int main() {
+	Arbre racine = noeud_initialisation(); 
+	noeud_insertion(racine, "hell"); 
+	printf("%d\n", recherche_mot_arbre(racine, "hello")); 
 	return 0; 
 }
 
 
+
+
+
+
+
+
+/*
+bool x = true;
+printf("%B\n", x);
+Ne marche pas 
+
+%d trasnforme le "bool" en 1 si vrai sinon 0 
+Implémnter de base ds ce type 
+
+*/
