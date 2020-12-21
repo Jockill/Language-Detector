9a6a045bd45e7324fc443e45479191fc7462ec7a
Wernert Thomas <thomas.wernert@etu.unistra.fr>
2020-12-21 15:29:12 +0100

    Ajout doxy dawg.h

--- header/dawg.h
+++ header/dawg.h
@@ -26,11 +26,10 @@
  \brief Structure représentatant un sommet du DAWG.
 */
 typedef struct Sommet {
-	int label; /*!< Label unique du sommet courant*/
-	struct Sommet *tab[N]; /*!< Arêtes sortantes du sommet. */
-	bool mot_valide; /*!< Mot présent dans le dictionnaire ? */
-   	size_t id;
-   	bool fin;
+	struct Sommet *tab[N]; 	/*!< Arêtes sortantes du sommet. */
+	bool mot_valide; 	/*!< Bool pur savoir si le mot actuel est dans le dictionnaire. */
+   	size_t id;		/*!< Représente l'id du sommet actuel. */
+   	bool fin;		/*!< Bool pour savoit si sommet est final. */
 } Sommet;
 
 /*!
@@ -48,31 +47,81 @@ typedef struct Arete {
  \brief Représente le dawg
 */
 typedef struct dawg {
-	Sommet *racine;
-	struct hashmap_s hashmap;
-	struct stack *pile;
-	size_t id_suivant;
-	char *dernier_sommet;
+	Sommet *racine;			/*!< Pointeur vers la racine du dawg. */
+	struct hashmap_s hashmap;	/*!< Hasmap associée au dawg actuel. */
+	struct stack *pile;		/*!< Pile associé au dawg actuel. */
+	size_t id_suivant; 		/*!< Id suivant. */
+	char *dernier_sommet; 		/*!< String représentant le dernier mot ajouté. */
 } dawg;
 /******************************************************************************/
 /******************************************************************************/
 
+/*!
+   \brief Initialisation du dawg.  
+   \return Un dawg
+*/
 dawg *initilisation_dawg(void);
 
+
+/*!
+   \brief Calcul le plus grand préfixe commun entre deux strings 
+   \param [in] pointeur. 
+   \param [in] char Pointeur. 
+   \return size_t. 
+*/
 size_t plusGrandPrefixeCommun(char* mot1, char* mot2);
 
+/*!
+   \brief Créer une clé à partir d'un sommet.
+   \param [in] Sommet Pointeur. 
+   \return Une string. 
+*/
 char *confection_cle(Sommet *sommet);
 
+/*!
+   \brief Minimasation jusqu'a une profondeur taille.
+   \param [in] dawg Pointeur vers un dawg. 
+   \param [in] size_t Taille qui représente la profondeur. 
+   \return None. 
+*/
 void minimisation(dawg *dawg, size_t taille);
 
+/*!
+   \brief Insérer un mot dans le dawg. 
+   \param [in] dawg Pointeur vers un dawg. 
+   \param [in] char Pointeur. 
+   \return None. 
+*/
 void sommet_insertion(dawg *dawg, char *mot);
 
+/*!
+   \brief Rechercher la présence d'un mot dans le dawg. 
+   \param [in] dawg Pointeur vers un dawg. 
+   \param [in] char Pointeur. 
+   \return bool. 
+*/
 bool recherche_mot_dawg(dawg *dawg, char *mot);
 
+/*!
+   \brief Libération de la hasmap associé à un dawg. 
+   \param [in] void Hashmap. 
+   \param [in] struct hasmap_element_s element. 
+   \return int. 
+*/
 int suppression_hasmap(void *const hashmap, struct hashmap_element_s *const element);
 
+/*!
+   \brief Libération de l'esapace mémoire allouer à un dawg. 
+   \param [in] dawg Pointeur vers un dawg. 
+   \return int. 
+*/
 void suppression_dawg(dawg *dawg);
 
+/*!
+   \brief Création d'un dawg avec un dictionnaire. 
+   \param [in] dawg Pointeur vers un dawg. 
+   \return int. 
+*/
 dawg *construct_dawg(const char *pathname);
 
 float dawg_test_mot_fr(dawg *racine, char *mot, int last);
@@ -83,48 +132,5 @@ float dawg_test_mot_germ(dawg *racine, char *mot, int last);
 
 void detec_dawg(char phrase[MAX_SIZE+1], int temps);
 
-
-
-
-
-
-
-
-
-
-
-
-// /*!
-//    \brief Minimise par mutation un DAWG jusqu'à la profondeur 'n'.
-//    \param [in] struct dawg_sommet racine Racine du DAWG.
-//    \param [in] size_t p Profondeur jusqu'à laquelle minimiser.
-//    \return NONE
-// */
-// void dawg_minimiser(struct dawg_sommet racine, size_t p);
-
-// /*!
-//    \brief Ajoute des noeuds au DAWG en fonction du mot passé en argument.
-//    \param [in] dawg_sommet* depart Point de départ de l'ajout.
-//    \param [in] char* mot Mot à insérer.
-//    \param [in] size_t indexDebut Index du mot à partir duquel commencer à ajouter.
-//    \return NONE
-// */
-// void dawg_ajouter(struct dawg_sommet* depart, char* mot, size_t indexDebut);
-
-// /*!
-// \brief Insere par mutation un mot dans le DAWG.
-// \param [in] struct dawg_sommet sommet racine du DAWG.
-// \param [in] char* motPrecedent Mot précédant 'mot'.
-// \param [in] char* mot Mot à inserer.
-// \return NONE
-// */
-// void dawg_inserer_mot(struct dawg_sommet racine, char* motPrecedent, char* mot);
-
-// /*!
-//    \brief Genere un DAWG avec un dictionnaire
-//    \param [in] char* pathToDic Chemin d'acces au dictionnaire à utiliser.
-//    \return Un DAWG rempli avec le dictionnaire.
-// */
-// struct dawg_sommet creation(char* pathToDic);
 
 #endif
